2b0baef6be37271b6f78f93aa8e895b10ecb2a5c
aliabid94 <aabid94@gmail.com>
2021-01-20 18:43:24 -0800

    Update run-gradio.py

--- run-gradio.py
+++ run-gradio.py
@@ -1,9 +1,14 @@
 from hello import hello 
 import gradio
+import time
 
 print("TESTING")
 
 io = gradio.Interface(fn=hello, inputs='text', outputs='text', verbose=True, title='Hello World', 
     description='Hello World Descriptiooon', thumbnail='https://github.com/gradio-app/gradio_static/blob/master/static/img/logo.png?raw=true', analytics_enabled=False)
+
+for i in range(20):
+    print(i)
+    time.sleep(1)
 
 io.launch(debug=True)
