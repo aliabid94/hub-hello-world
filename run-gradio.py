84e612f6ab4e1bef4c6f665dba136ac6ccc2bd61
aliabid94 <aabid94@gmail.com>
2021-06-23 11:58:58 -0700

    Update run-gradio.py

--- run-gradio.py
+++ run-gradio.py
@@ -1,8 +1,15 @@
-from hello import hello 
 import gradio
 import time
+import torch
 
 print("TESTING")
+
+def hello(inp):
+  if inp == "fail":
+    raise ValueError("whoops") 
+  elif inp == "gpu":
+    return torch.cuda.device_count()
+  return inp.replace("hello", "HELLOO").replace("Hello", "HELLOO")
 
 io = gradio.Interface(fn=hello, inputs='text', outputs='text', verbose=True, title='Hello World', 
     description='Hello World Descriptiooon', thumbnail='https://github.com/gradio-app/gradio_static/blob/master/static/img/logo.png?raw=true', analytics_enabled=False)
